b52cf0ac021d8b26f00f992bf0378559947ebc53
re-knownout <knownout@hotmail.com>
2020-07-09 21:28:12 +0300

    Book parser and specs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import "normalize.css";
 import "./root.less";
+import BookParser from "./fragments/book-parser";
 
 window.addEventListener("load", async () => {
 	const dialog = document.createElement("input");
 	dialog.type = "file";
 
-	dialog.onchange = async () => {};
+	dialog.onchange = () => new BookParser().loadBookData(dialog.files).then(console.log);
 	window.onclick = () => dialog.click();
 });
