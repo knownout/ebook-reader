93ed2e6c6e83a4d11c41467f18d0d993eabaceab
re-knownout <knownout@hotmail.com>
2021-07-18 21:27:55 +0300

    Exclude tags from the text cleaning procedure

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,13 +1,38 @@
+const defaultExcludeTags = [ "b", "bold", "strong", "s", "u", "i" ];
+
+/**
+ * A procedure that invokes a callback if the browser supports extended regex options
+ * @param callback function to be called if the browser supports extended regex
+ */
+const whenRegexSupported = (callback: () => any): void =>
+	'<tag ns="nf">'.replace(/(?<=<\w+)\s+[^>]+/gi, "") === "<tag>" ? callback() : null;
+
 /**
  * Function to remove HTML tags and extra spaces from text
  * @param rawText input text
  * @returns clean text
  */
-export function getCleanText (rawText: string): string {
-	// FIXME regex optimization
+export function getCleanText (rawText: string, excludeTags: string[] = defaultExcludeTags): string {
 	let text = rawText.replace(/&lt;/gi, "<").replace(/&gt;/gi, ">");
-	text = text.replace(/(<([^>]+)>)/gi, "").replace(/\s{2,}/gi, " ");
 
+	// BUG positive lookbehind may not be supported
+	// FIXME optimization for this part
+
+	// BUG Tags will only be excluded if extended regex options are supported by the client browser
+	whenRegexSupported(() => {
+		// Removing all html tag parameters
+		text = text.replace(/(?<=<\w+)\s+[^>]+/gi, "");
+
+		// Replacing brackets with square brackets for excluded tags
+		excludeTags.forEach(tag => {
+			text = text
+				.replace(new RegExp(`<${tag}>`, "g"), `[${tag}]`)
+				.replace(new RegExp(`</${tag}>`, "g"), `[/${tag}]`);
+		});
+	});
+
+	// Removing all tags from text
+	text = text.replace(/(<([^>]+)>)/gi, "").replace(/\s{2,}/gi, " ");
 	return text.trim();
 }
 
